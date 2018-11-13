43fbe574e21cd60a12149bea742d34c0cc6b6612
EthanButtery <ethanbuttery@gmail.com>
2018-11-13 13:11:47 -0500

    Login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(username: params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      if user.admin == true
+        redirect_to admin_path
+      else
+        redirect_to employee_path
+      end
+    else
+      flash.now[:danger] = 'Invalid username/password combination'
+      render 'new'
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_url
+  end
+end
