888fbdf7ec702019853f5376ed5e37654ee73ee9
Justin Inho <justininho@my.ccsu.edu>
2018-11-16 18:30:38 +0000

    removed branding

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :servers
   resources :users
+
   get '/admin',    to: 'static_pages#admin'
   get '/employee',  to: 'static_pages#employee'
   root 'sessions#new'
@@ -9,5 +10,6 @@ Rails.application.routes.draw do
   delete '/logout', to: 'sessions#destroy'
   get 'dashboard/',  to: 'dashboard#index'
   get 'servers/index', to: 'servers#index'
+  get '/server', to: 'server#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
