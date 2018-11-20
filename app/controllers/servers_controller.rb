c0e9a5e309997d4c27699f90c6e4121e1e0defb6
EC2 Default User <ec2-user@ip-172-31-11-74.us-east-2.compute.internal>
2018-11-20 04:34:03 +0000

    added search function in server and employee page

--- app/controllers/servers_controller.rb
+++ app/controllers/servers_controller.rb
@@ -4,7 +4,7 @@ class ServersController < ApplicationController
   # GET /servers
   # GET /servers.json
   def index
-    @servers = Server.all
+      @servers = Server.where(["hostname LIKE ?", "%#{params[:search]}%"])
   end
 
   # GET /servers/1
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   def index
-    @users = User.all
+    @users = User.where(["username LIKE ?", "%#{params[:search]}%"])
   end
   
   # GET /users/1
