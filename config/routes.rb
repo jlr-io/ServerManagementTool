155238c78fe7b3d0858b4e7b448cafab9efdd438
EC2 Default User <ec2-user@ip-172-31-19-244.us-east-2.compute.internal>
2018-11-27 17:23:25 +0000

    pre-pull commit

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,12 @@ Rails.application.routes.draw do
 
   get '/admin',    to: 'static_pages#admin'
   get '/employee',  to: 'static_pages#employee'
-  root 'sessions#new'
+  root 'sessions#new'  
   get '/login',     to: 'sessions#new'
   post '/login',    to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   get '/dashboard',  to: 'dashboard#index'
+  get '/requests', to: 'requests#edit'
   get 'servers/index', to: 'servers#index'
   get '/server', to: 'server#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
