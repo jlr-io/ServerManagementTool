937e5322c4ba35446840465aac56d40575559a3b
Justin Inho <justininho@my.ccsu.edu>
2018-12-03 18:49:38 +0000

    changed systems table to have memory attribute instead of machine

--- db/migrate/20181129193321_create_join_table_tickets_users.rb
+++ db/migrate/20181129193321_create_join_table_tickets_users.rb
@@ -1,8 +1,8 @@
 class CreateJoinTableTicketsUsers < ActiveRecord::Migration[5.2]
   def change
     create_join_table :tickets, :users do |t|
-      # t.index [:ticket_id, :user_id]
-      # t.index [:user_id, :ticket_id]
+    t.index [:ticket_id, :user_id]
+    t.index [:user_id, :ticket_id]
     end
   end
 end
