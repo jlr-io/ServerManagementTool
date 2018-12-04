c34ad8fdf625ed485acc031dbc83eed19f448a06
Justin Inho <justininho@my.ccsu.edu>
2018-12-04 17:45:05 +0000

    fixed background in server show. Also search view results. And email fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -89,7 +89,7 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:id, :username, :admin, :password, :password_confirmation)
+      params.require(:user).permit(:id, :username, :admin, :password, :password_confirmation, :email)
     end
     
     
